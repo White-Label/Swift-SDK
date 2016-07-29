7812e2b8b8429e403fb723c1d2370d3bac6abdcc
Alexander Givens <mail@alexgivens.com>
2016-07-28 21:05:32 -0700

    Fix pod includes

---
+++ WhiteLabel/Classes/WhiteLabel-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  WhiteLabel-Bridging-Header.h
+//  Pods
+//
+//  Created by Alexander Givens on 7/28/16.
+//
+//
+
+#ifndef WhiteLabel_Bridging_Header_h
+#define WhiteLabel_Bridging_Header_h
+
+
+#endif /* WhiteLabel_Bridging_Header_h */
